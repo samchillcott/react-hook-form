7b0d3390e3ed36cf487b4d776c6d9fd968ad86d0
Sam Chillcott <samchillcott@gmail.com>
2021-06-22 14:13:18 +0100

    update eslint config + add basic form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <form action="">
+      <input type="text" placeholder="Email" name="email" />
+      <input type="password" placeholder="password" name="password" />
+      <input type="submit" />
+    </form>
   );
 }
 
