e954ecf21f446e11353363284fc674ded0551d54
Sam Chillcott <samchillcott@gmail.com>
2021-06-29 11:20:56 +0100

    add first and last name fields

--- src/App.js
+++ src/App.js
@@ -5,26 +5,42 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
-  email: yup.string().email().required(),
+  firstName: yup.string().required('First Name is a required field'),
+  lastName: yup.string().required('Last Name is a required field'),
+  email: yup.string().email().required('Email is a required field'),
   password: yup.string().required().matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
-    'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
+    'Password Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
   ),
 });
 
 export default function App() {
   const {
-    register, handleSubmit, watch, formState: { errors },
+    register, handleSubmit, formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
   const onSubmit = (data) => console.log(data);
 
-  console.log(watch());
-  console.log(errors);
+  // console.log(watch());
+  // console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="firstName">
+        First Name
+        <input id="firstName" {...register('firstName')} />
+        <p>
+          {errors.firstName?.message}
+        </p>
+      </label>
+      <label htmlFor="lastName">
+        Last Name
+        <input id="lastName" {...register('lastName')} />
+        <p>
+          {errors.lastName?.message}
+        </p>
+      </label>
       <label htmlFor="email">
         Email Address
         <input id="email" {...register('email')} />
